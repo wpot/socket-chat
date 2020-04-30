af7f2cc981e538e710233227b779f87202871d7e
omar wpot <omarwpot100@gmail.com>
2020-04-30 18:58:03 -0500

    chat v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,29 +3,25 @@ var socket = io();
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-
     window.location = 'index.html';
-
-    throw new Error(' el nombre es necesario, tambien la sala');
-
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
+};
 
-}
 
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
 
-        console.log('usuarios conectados', resp);
-
+        renderizarUsuarios(resp);
     });
-
 
 });
 
@@ -38,45 +34,28 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
-
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom()
 });
 
-// escuchar cuando un usuario entra y sale
-
-
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-
-
-//mensajes privados
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('mensaje privado:', mensaje);
-
+    console.log('Mensaje Privado:', mensaje);
 
 });
